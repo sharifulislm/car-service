803a7d268ba81b357d09a71cc030685a0cece867
sharifulislm <shariful.islam.coder@gmail.com>
2022-05-03 20:44:41 +0300

    set a private asios

--- src/Pages/Order/Order.js
+++ src/Pages/Order/Order.js
@@ -1,19 +1,35 @@
-import axios from 'axios';
+
+import { signOut } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import axiosPrivate from '../../Api/axiosPrivate';
+
 import auth from '../../firebase.init';
 
 const Order = () => {
     const [user] = useAuthState(auth);
     const [orders, setOrders] = useState([]);
+    const navigate = useNavigate();
 
     useEffect( () => {
       
         const getOrders = async() => {
             const email = user.email;
             const url = `http://localhost:5000/order?email=${email}`;
-            const {data} = await axios.get(url);
-            setOrders(data);
+          
+            try {
+                const {data} = await axiosPrivate.get(url);
+             setOrders(data);
+            }
+            catch(error){
+                console.log(error.message);
+                if(error.response.status ===401 || error.response.status ===403) {
+                   signOut(auth);
+                    navigate('/login')
+                }
+            }
+
         }
         getOrders();
 
